a8edb2f26be09ba8e522bc99d03843d2686b83c4
Srinivas6281 <166312848+Srinivas6281@users.noreply.github.com>
2025-04-08 13:20:31 +0530

    Update About.jsx
    effects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,17 +6,17 @@ const About = forwardRef((props,ref) => {
   return (
     <div className='about' ref={ref}  data-aos={props['data-aos']}>
         <div className="about-me">
-            <h1>About Me</h1>
+            <h1 data-aos={props['data-aos']}>About Me</h1>
             <div className="con1">
-               <p> <img src={arrow} alt="" />  Hi , I'm Shiva, a dedicated home appliance repair specialist with over 5 years of experience at Samsung.
+               <p data-aos={props['data-aos']}> <img src={arrow} alt="" />  Hi , I'm Shiva, a dedicated home appliance repair specialist with over 5 years of experience at Samsung.
                I specialize in repairing and servicing a wide range of home appliances including <span>air conditioner, refrigerators, washing machines, and televisions</span>
                </p><br />
-               <p><img src={arrow} alt="" /> 
+               <p data-aos={props['data-aos']}><img src={arrow} alt="" /> 
                  Over the years, I've built a reputation for <span>reliable service, honest work, </span>and <span>fast problem-solving.</span>
                 My goal is simple to make sure your appliances work smoothly and your home runs stress-free.
                </p><br />
-               <p> <img src={arrow} alt="" /> Whether it's cooling issue in your fridge, a noisy washing machine, or an unresponsive TV, you can contact me to get it fixed with professional precision and care.</p><br />
-               <p><img src={arrow} alt="" />  Need help with your appliance? Just reach out-I'm here to help!</p>
+               <p data-aos={props['data-aos']}> <img src={arrow} alt="" /> Whether it's cooling issue in your fridge, a noisy washing machine, or an unresponsive TV, you can contact me to get it fixed with professional precision and care.</p><br />
+               <p data-aos={props['data-aos']}><img src={arrow} alt="" />  Need help with your appliance? Just reach out-I'm here to help!</p>
             </div>
         </div>
     </div>
